543d3e054b542182c8b1d8a7d8eac4cea843e78b
Dusty <42273218+DustyStudy@users.noreply.github.com>
2026-08-24 19:15:59 -0500

    Updates

--- terraform/security-baseline-new-accounts/organization-trail/main.tf
+++ terraform/security-baseline-new-accounts/organization-trail/main.tf
@@ -6,7 +6,6 @@
 
 data "aws_partition" "current" {}
 data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
 
 locals {
   # A syntactically-valid but non-matching org ID, so the decrypt
